d0ed994c034a0cf94dbea5a07c89529be36a42b8
Ryleigh Newman <ryleighn@gmail.com>
2024-07-31 15:59:28 -0400

    Update index.html, styles.css, and add new files

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Additional JavaScript functionality can be added here
